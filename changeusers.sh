751b4132d9845c99b6a991729b3c740768231bf3
Dan Bedard <danb@renci.org>
2014-09-26 11:20:23 -0400

    Changed ' to " in last sed, so variables get dereferenced.

--- changeusers.sh
+++ changeusers.sh
@@ -8,8 +8,8 @@ NEW_A=$3
 NEW_B=$4
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 sed -i "s/$OLD_A/$NEW_A/g" $DIR/www/scripts/fza.sh 
-sed -i "s/$OLD_A/$NEW_A/g" $DIR/www/scripts/addfiles_zoneA.sh 
-sed -i "s/$OLD_A/$NEW_A/g" $DIR/www/scripts/irodsEnv.USER_A 
+sed -i "s/$OLD_A/$NEW_A/g" $DIR/www/scripts/addfiles_zoneA.sh
+sed -i "s/$OLD_A/$NEW_A/g" $DIR/www/scripts/irodsEnv.USER_A
 sed -i "s/$OLD_A/$NEW_A/g" $DIR/www/scripts/reset_zoneA.sh
 sed -i "s/$OLD_A/$NEW_A/g" $DIR/www/html/thumbnails/index.php
 sed -i "s/$OLD_A/$NEW_A/g" $DIR/www/html/index.html
@@ -17,9 +17,4 @@ sed -i "s/$OLD_B/$NEW_B/g" $DIR/www/scripts/fza.sh
 sed -i "s/$OLD_B/$NEW_B/g" $DIR/www/scripts/addfiles_zoneB.sh
 sed -i "s/$OLD_B/$NEW_B/g" $DIR/www/scripts/irodsEnv.USER_B
 sed -i "s/$OLD_B/$NEW_B/g" $DIR/www/scripts/reset_zoneB.sh
-sed -i 's/$OLD_B/$NEW_B/g' $DIR/www/html/index.html
-
-
-
-
- 
+sed -i "s/$OLD_B/$NEW_B/g" $DIR/www/html/index.html
