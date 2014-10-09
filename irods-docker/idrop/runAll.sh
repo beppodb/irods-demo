9497c27fc7f151c5212cd150642e35537a04c476
Dan Bedard <beppodb@users.noreply.github.com>
2014-10-09 17:13:22 -0400

    Changing reset to be zonename specific so they don't collide.

--- irods-docker/idrop/runAll.sh
+++ irods-docker/idrop/runAll.sh
@@ -11,8 +11,7 @@ su -c 'sed -i "s/tempZone/$ZONENAME/" /var/lib/irods/.irods/.irodsEnv' irods
 echo "http://$hostsname:$port8443/idrop-web2" > /var/www/html/thumbnails/stub
 /home/admin/update_idwconfig.sh /etc/idrop-web/idrop-web-config2.groovy
 supervisorctl start tomcat6
-/var/www/scripts/reset_zoneA.sh
-/var/www/scripts/reset_zoneB.sh
+/var/www/scripts/reset_$ZONENAME.sh
 
 
 
