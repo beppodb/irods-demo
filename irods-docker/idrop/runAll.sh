28459872a2908a721ccf0f19f855fae92fc0de7b
Dan Bedard <beppodb@users.noreply.github.com>
2014-10-09 16:44:41 -0400

    Adding zone resets to the start script.

--- irods-docker/idrop/runAll.sh
+++ irods-docker/idrop/runAll.sh
@@ -11,6 +11,9 @@ su -c 'sed -i "s/tempZone/$ZONENAME/" /var/lib/irods/.irods/.irodsEnv' irods
 echo "http://$hostsname:$port8443/idrop-web2" > /var/www/html/thumbnails/stub
 /home/admin/update_idwconfig.sh /etc/idrop-web/idrop-web-config2.groovy
 supervisorctl start tomcat6
+/var/www/scripts/reset_zoneA.sh
+/var/www/scripts/reset_zoneB.sh
+
 
 
 
